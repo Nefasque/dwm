98dd3544ef45144293c552ed049ead7b9ffc43ae
Nefasque <Pedro0fici4lDev@gmail.com>
2024-01-07 18:07:08 -0400

    gat to 5px

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static unsigned int borderpx  = 2;        /* border pixel of windows */
-static unsigned int gappx     = 10;        /* gaps between windows */
+static unsigned int gappx     = 3;        /* gaps between windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
